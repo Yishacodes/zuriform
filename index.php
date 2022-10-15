4601fcd48e84019422c2a54602de36081ece4f91
yishacodes <olatunjiaisha21@gmail.com>
2022-10-15 10:45:26 +0100

    changes

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>registration form<h1>
+        <form method="post" action="user_data.php">
+        <label for="name">
+       Name: <input type="text" id="name" name="name">
+</label><br>
+<label for="email">
+       Email: <input type="email" id="email" name="email">
+</label><br>
+<label for="birthday">
+       Name: <input type="date" id="birthday" name="date">
+</label><br>
+<label for="gender">
+       Name: <input type="text" id="gender" name="gender">
+</label><br>
+<label for="country">
+       Name: <input type="text"  id="country" name="country">
+</label>
+<button type="submit">submit</button>
+</body>
+</html>
