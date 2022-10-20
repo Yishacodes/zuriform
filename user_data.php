0614dfd9b0aa799f832e333a95484edaab587576
yishacodes <olatunjiaisha21@gmail.com>
2022-10-20 20:33:07 +0100

    changes

--- user_data.php
+++ user_data.php
@@ -5,7 +5,9 @@ $date=$_POST['date'];
 $gender=$_POST['gender'];
 $country=$_POST['country'];
 
+echo print_r($_POST)
 
+/*
 echo "NAME: " . $name  ." <br>";
 
 echo "EMAIL: " . $email  ." <br>";
@@ -15,10 +17,12 @@ echo "DATE: " . $date  ." <br>";
 echo "GENDER: " . $gender  ." <br>";
 
 echo "COUNTRY: " . $country ." <br>";
+*/
 
-$filename="./user_data.csv";
-$handle=fopen($filename,"w");
-fwrite($handle, '$name');
+
+$_POST="./user_data.csv";
+$handle=fopen($_POST,"w");
+fwrite($handle, '$_POST[name,);
 fclose($handle)
 
 
